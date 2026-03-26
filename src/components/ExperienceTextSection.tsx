1f869903ced1cbbe7c56dd52d349671f37b1598b
Anton Kolesnikov <sterlet.prod@gmail.com>
2026-03-26 21:47:30 +0000

    обычный текст 3vw, "5 costly mistakes" - 3.1vw

--- src/components/ExperienceTextSection.tsx
+++ src/components/ExperienceTextSection.tsx
@@ -10,7 +10,7 @@ const part1 = [
 
 const part2 = [
   "It’s not the traffic or the budget — it’s",
-  "<span class='font-kurale font-bold text-[#c7b684]'>5 costly mistakes</span> web designers make",
+  "<span class='font-kurale font-bold text-[#c7b684] text-[3.1vw]'>5 costly mistakes</span> web designers make",
   "when they focus on looks instead of",
   "<span class='font-kurale font-bold text-[#c7b684] text-[3.1vw]'>conversions</span>"
 ];
@@ -24,7 +24,7 @@ export function ExperienceTextSection() {
           <HighlightWipeHeading 
             as="p"
             lines={part1}
-            className="text-xl sm:text-2xl md:text-[2vw] font-medium uppercase leading-[1.3] md:leading-[1.1] text-[#e0ded8] items-start text-left tracking-tighter"
+            className="text-xl sm:text-2xl md:text-[3vw] font-medium uppercase leading-[1.3] md:leading-[1.1] text-[#e0ded8] items-start text-left tracking-tighter"
             stagger={0.08}
             triggerOnce={true}
           />
@@ -35,7 +35,7 @@ export function ExperienceTextSection() {
           <HighlightWipeHeading 
             as="p"
             lines={part2}
-            className="text-xl sm:text-2xl md:text-[2vw] font-medium uppercase leading-[1.3] md:leading-[1.1] text-[#e0ded8] items-end text-right tracking-tighter"
+            className="text-xl sm:text-2xl md:text-[3vw] font-medium uppercase leading-[1.3] md:leading-[1.1] text-[#e0ded8] items-end text-right tracking-tighter"
             stagger={0.08}
             triggerOnce={true}
             delay={0.4}
