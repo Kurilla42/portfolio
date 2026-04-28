15689f8cece12e7700df65323dcaf336e0b82ae5
Anton Kolesnikov <sterlet.prod@gmail.com>
2026-04-28 15:32:38 +0000

    а ты учитываешь что у картинок есть еще рамка в виде браузера, они и нае

--- src/components/SiteShowcaseSection.tsx
+++ src/components/SiteShowcaseSection.tsx
@@ -40,14 +40,14 @@ export function SiteShowcaseSection() {
 
   const headingOpacity = useTransform(scrollYProgress, [0, 0.1], [1, 0]);
 
-  // Настройка анимаций для появления (уменьшен диапазон Y чтобы не было нахлеста)
-  const case1Y = useTransform(scrollYProgress, [0.1, 0.3], ["40vh", "0vh"]);
+  // Скорректированные диапазоны: уменьшена амплитуда Y с 40vh до 20vh для предотвращения нахлеста
+  const case1Y = useTransform(scrollYProgress, [0.1, 0.3], ["20vh", "0vh"]);
   const case1Opacity = useTransform(scrollYProgress, [0.1, 0.25], [0, 1]);
 
-  const case2Y = useTransform(scrollYProgress, [0.35, 0.55], ["40vh", "0vh"]);
+  const case2Y = useTransform(scrollYProgress, [0.35, 0.55], ["20vh", "0vh"]);
   const case2Opacity = useTransform(scrollYProgress, [0.35, 0.5], [0, 1]);
 
-  const case3Y = useTransform(scrollYProgress, [0.6, 0.8], ["40vh", "0vh"]);
+  const case3Y = useTransform(scrollYProgress, [0.6, 0.8], ["20vh", "0vh"]);
   const case3Opacity = useTransform(scrollYProgress, [0.6, 0.75], [0, 1]);
 
   const caseTransforms = [
